14ab9c4321af44c10013abca4bf895982b77a4d4
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-04-13 20:42:47 +0100

    Step 2 - Tweak tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -21,7 +21,7 @@ it('GET /jokes should respond with a jokes message', done => {
     });
 });
 
-it('GET /joke/random should respond with a jokes message', done => {
+it('GET /joke/random should respond with a random joke message', done => {
   request(app)
     .get('/joke/random')
     .then(res => {
@@ -33,7 +33,7 @@ it('GET /joke/random should respond with a jokes message', done => {
 
 it('GET /joke/random/personal should respond with a personal joke message', done => {
   request(app)
-    .get('/joke/random/manchester/codes')
+    .get('/joke/personal/manchester/codes')
     .then(res => {
       expect(res.statusCode).toEqual(200);
       expect(res.body.message).toEqual('Reached the personal joke endpoint');
