747e9dce2ea8519b21721be740b72a7abcd4bf7b
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-04-21 15:17:03 +0100

    Step 8 - Refactor to use promises

--- src/controllers.js
+++ src/controllers.js
@@ -1,17 +1,10 @@
-// eslint-disable-next-line
-const request = require('request');
 const axios = require('axios');
 
 const jokesController = (req, res) =>
-  request('https://api.icndb.com/jokes', (error, jokesApiResponse) => {
-    if (error) {
-      return res.status(error.statusCode).send({ error: error.message });
-    }
-
-    const parsedResponse = JSON.parse(jokesApiResponse.body);
-
-    res.send({ jokes: parsedResponse.value });
-  });
+  axios
+    .get('https://api.icndb.com/jokes')
+    .then(response => res.send({ jokes: response.data.value }))
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 
 const randomJokeController = (req, res) =>
   axios
@@ -22,15 +15,12 @@ const randomJokeController = (req, res) =>
 const personalJokeController = async (req, res) => {
   const { first, last } = req.params;
 
-  try {
-    const response = await axios.get(
+  return axios
+    .get(
       `https://api.icndb.com/jokes/random?firstName=${first}&lastName=${last}&exclude=[explicit]`,
-    );
-
-    return res.send({ personalJoke: response.data.value });
-  } catch (error) {
-    return res.status(error.statusCode).send({ error: error.message });
-  }
+    )
+    .then(response => res.send({ personalJoke: response.data.value }))
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 };
 
 module.exports = {
