b1436a79ab9fcc8f1d1ddf0b17769f688eb760be
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-04-21 15:16:13 +0100

    Step 8 - Create test data folder

---
+++ data/test-data.js
@@ -0,0 +1,35 @@
+const mockListOfJokes = {
+  type: 'success',
+  value: [
+    {
+      id: 1,
+      joke: 'i am a joke',
+      categories: [],
+    },
+    {
+      id: 2,
+      joke: 'i am another joke',
+      categories: [],
+    },
+  ],
+};
+
+const mockRandomJoke = {
+  type: 'success',
+  value: {
+    id: 115,
+    joke: 'i am a random joke',
+    categories: [],
+  },
+};
+
+const mockPersonalJoke = {
+  type: 'success',
+  value: {
+    id: 141,
+    joke: 'random joke about manchester codes',
+    categories: [],
+  },
+};
+
+export { mockListOfJokes, mockRandomJoke, mockPersonalJoke };
