24eee55e7d07e5e82cbe77246c109894f6da418b
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-04-21 13:03:27 +0100

    Step 6 - Update remaining tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -58,7 +58,7 @@ describe('GET /jokes', () => {
       });
   });
 
-  it('should responde with an error message if something goes wrong', done => {
+  it('should respond with an error message if something goes wrong', done => {
     nock('https://api.icndb.com')
       .get('/jokes')
       .replyWithError({ statusCode: 500, message: 'huge error' });
@@ -73,49 +73,86 @@ describe('GET /jokes', () => {
   });
 });
 
-it('GET /joke/random should respond with a random joke message', done => {
-  const mockResponse = {
-    type: 'success',
-    value: {
-      id: 115,
-      joke: 'i am a random joke',
-      categories: [],
-    },
-  };
+describe('GET /joke/random', () => {
+  it('should respond with a random joke message', done => {
+    const mockResponse = {
+      type: 'success',
+      value: {
+        id: 115,
+        joke: 'i am a random joke',
+        categories: [],
+      },
+    };
 
-  nock('https://api.icndb.com')
-    .get('/jokes/random')
-    .query({ exclude: '[explicit]' })
-    .reply(200, mockResponse);
+    nock('https://api.icndb.com')
+      .get('/jokes/random')
+      .query({ exclude: '[explicit]' })
+      .reply(200, mockResponse);
 
-  request(app)
-    .get('/joke/random')
-    .then(res => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.randomJoke).toEqual({ categories: [], id: 115, joke: 'i am a random joke' });
-      done();
-    });
+    request(app)
+      .get('/joke/random')
+      .then(res => {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.randomJoke).toEqual({
+          categories: [],
+          id: 115,
+          joke: 'i am a random joke',
+        });
+        done();
+      });
+  });
+
+  it('should respond with an error message if something goes wrong', done => {
+    nock('https://api.icndb.com')
+      .get('/jokes/random')
+      .query({ exclude: '[explicit]' })
+      .replyWithError({ statusCode: 404, message: 'Unknown resource' });
+
+    request(app)
+      .get('/joke/random')
+      .then(res => {
+        expect(res.statusCode).toEqual(404);
+        expect(res.body.error).toEqual('Unknown resource');
+        done();
+      });
+  });
 });
 
-it('GET /joke/random/personal should respond with a personal joke message', async () => {
-  const mockResponse = {
-    type: 'success',
-    value: {
-      id: 141,
-      joke: 'random joke about manchester codes',
-      categories: [],
-    },
-  };
+describe('GET /joke/random/personal', () => {
+  it('should respond with a personal joke message', async () => {
+    const mockResponse = {
+      type: 'success',
+      value: {
+        id: 141,
+        joke: 'random joke about manchester codes',
+        categories: [],
+      },
+    };
 
-  nock('https://api.icndb.com')
-    .get('/jokes/random')
-    .query({ exclude: '[explicit]', firstName: 'manchester', lastName: 'codes' })
-    .reply(200, mockResponse);
+    nock('https://api.icndb.com')
+      .get('/jokes/random')
+      .query({ exclude: '[explicit]', firstName: 'manchester', lastName: 'codes' })
+      .reply(200, mockResponse);
 
-  request(app)
-    .get('/joke/personal/manchester/codes')
-    .then(res => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.personalJoke).toEqual(mockResponse.value);
-    });
+    request(app)
+      .get('/joke/personal/manchester/codes')
+      .then(res => {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.personalJoke).toEqual(mockResponse.value);
+      });
+  });
+
+  it('should respond with an error if something goes wrong', async () => {
+    nock('https://api.icndb.com')
+      .get('/jokes/random')
+      .query({ exclude: '[explicit]', firstName: 'manchester', lastName: 'codes' })
+      .replyWithError({ statusCode: 500, message: 'Bad request' });
+
+    request(app)
+      .get('/joke/personal/manchester/codes')
+      .then(res => {
+        expect(res.statusCode).toEqual(500);
+        expect(res.body.error).toEqual('Bad request');
+      });
+  });
 });
